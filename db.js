7afc3bf7cb54a429f42ae2096f571c1d777a90c1
Oluwafemi <oluwafemiomolounnu@gmail.com>
2023-04-15 23:43:30 +0100

    updated env

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
-import mysql from "mysql"
+import mysql from "mysql";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "PASSWORD_DB",
+    password: "process.env.PASSWORD_DB",
     database: "gibhorizon"
 })
